64bc19581986059983c016ade446bdfd931f7aa8
arnavbhatiamait <arnavbhatiamait@gmail.com>
2024-03-24 12:45:40 +0530

    initial commit

---
+++ SVM kernel file.py
@@ -0,0 +1,174 @@
+# %% [markdown]
+# Kernel SVM
+
+# %% [markdown]
+# Importing The Libraries
+
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %% [markdown]
+# Importing The Data Set
+
+# %%
+df=pd.read_csv("Social_Network_Ads.csv")
+df
+
+# %%
+x=df.iloc[:,:-1].values
+x
+
+# %%
+y=df.iloc[:,-1].values
+y
+
+# %% [markdown]
+# Splitting the data into Train and Test Set
+
+# %%
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=0,test_size=0.25)
+print(x_train,x_test,y_train,y_test)
+
+# %% [markdown]
+# Scaling the data set
+
+# %%
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.fit_transform(x_test)
+print(x_train,x_test)
+
+# %% [markdown]
+# Fitting the data set on the training set
+
+# %%
+from sklearn.svm import SVC
+classifier=SVC(kernel="rbf",random_state=0)
+classifier.fit(x_train,y_train)
+
+
+# %% [markdown]
+# Prediction of a new result
+
+# %%
+print(classifier.predict(sc.fit_transform([[30,87000]])))
+
+# %% [markdown]
+# Prediction of test set
+
+# %%
+y_pred = classifier.predict(x_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+
+# %% [markdown]
+# Confussion Matrix
+
+# %%
+from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
+import seaborn as sns
+cm= confusion_matrix(y_test,y_pred)
+print(cm)
+sns.heatmap(cm,annot=True,fmt='g')
+plt.xlabel("Actual value")
+plt.ylabel("Predicted value")
+plt.title("Confussion Matrix")
+plt.show()
+
+# %% [markdown]
+# Accuracy Score
+
+# %%
+print(accuracy_score(y_test,y_pred))
+
+# %% [markdown]
+# Classification Report
+
+# %%
+print(classification_report(y_test,y_pred))
+
+# %% [markdown]
+# Visualizing The training Set Results
+
+# %%
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),alpha= 0.75, cmap = ListedColormap(('red', 'green')))
+# print(X1)
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+plt.savefig("Training Set.png")
+
+# %%
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as mtp
+import numpy as nm
+x_set, y_set = x_train, y_train  
+x1, x2 = nm.meshgrid(nm.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step  =0.01),  
+nm.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
+mtp.contourf(x1, x2, classifier.predict(nm.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),  
+alpha = 0.75, cmap = ListedColormap(('blue','pink')))  
+mtp.xlim(x1.min(), x1.max())  
+mtp.ylim(x2.min(), x2.max())  
+# print(x1)
+for i, j in enumerate(nm.unique(y_set)):  
+    mtp.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
+        c = ListedColormap(('blue','pink' ))(i), label = j)  
+mtp.title('SVM classifier (Training set)')  
+mtp.xlabel('Age')  
+mtp.ylabel('Estimated Salary')  
+mtp.legend()  
+mtp.show()  
+plt.savefig("Train.png")
+
+# %% [markdown]
+# Visualizing The Test Set Results
+
+# %%
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+plt.savefig("Testing Set.png")
+
+
+# %%
+from matplotlib.colors import ListedColormap  
+x_set, y_set = x_test, y_test  
+x1, x2 = nm.meshgrid(nm.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step  =0.01),  
+nm.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
+mtp.contourf(x1, x2, classifier.predict(nm.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),  
+alpha = 0.75, cmap = ListedColormap(('blue','pink' )))  
+mtp.xlim(x1.min(), x1.max())  
+mtp.ylim(x2.min(), x2.max())  
+for i, j in enumerate(nm.unique(y_set)):  
+    mtp.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
+        c = ListedColormap(('blue','pink' ))(i), label = j)  
+mtp.title('SVM classifier (Test set)')  
+mtp.xlabel('Age')  
+mtp.ylabel('Estimated Salary')  
+mtp.legend()  
+mtp.show()  
+plt.savefig("test.png")
+
+
