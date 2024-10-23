90de3690660d94862667d4cfd80117149f0de4ab
arnavbhatiamait <arnavbhatiamait@gmail.com>
2024-10-23 18:22:37 +0530

    commit 23-10-2024

--- kernel SVM.py
+++ kernel SVM.py
@@ -155,7 +155,7 @@ x_set, y_set = x_test, y_test
 x1, x2 = nm.meshgrid(nm.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step  =0.01),  
 nm.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
 mtp.contourf(x1, x2, classifier.predict(nm.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),  
-alpha = 0.75, cmap = ListedColormap(('red','green' )))  
+alpha = 0.75, cmap = ListedColormap(("red","green" )))
 mtp.xlim(x1.min(), x1.max())  
 mtp.ylim(x2.min(), x2.max())  
 for i, j in enumerate(nm.unique(y_set)):  
